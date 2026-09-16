e48335384f1c63759ea1b838a8cf0f34c216e28c
E.Ermolova <lizavetaermolova421@gmail.com>
2026-09-16 14:57:02 +0700

    Labs2

--- lab2/Lab2/src/task3.java
+++ lab2/Lab2/src/task3.java
@@ -1,2 +1,21 @@
 public class task3 {
+    public static void main(String[] args){
+        Bell bell = new Bell();
+
+        bell.sound();
+        bell.sound();
+    }
+}
+
+class Bell {
+    private boolean isDing = true;
+    public void sound(){
+        if(isDing){
+            System.out.println("ding");
+        }
+        else{
+            System.out.println("dong");
+        }
+        isDing = !isDing;
+    }
 }
