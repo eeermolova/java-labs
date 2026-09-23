e55e68f473cc4b19bdd8129ae2dce2a312d29e01
E.Ermolova <lizavetaermolova421@gmail.com>
2026-09-23 14:20:41 +0700

    the begining task6

---
+++ lab2/Lab2/src/exceptions/InvalidFigureException.java
@@ -0,0 +1,7 @@
+package exceptions;
+
+public class InvalidFigureException extends Exception{
+    public InvalidFigureException(String msg){
+        super(msg);
+    }
+}
---
+++ lab2/Lab2/src/geometry2d/Circle.java
@@ -0,0 +1,19 @@
+package geometry2d;
+
+public class Circle implements Figure{
+
+    @Override
+    public double area() {
+        return 0;
+    }
+
+    @Override
+    public double perimeter() {
+        return 0;
+    }
+
+    @Override
+    public String toString(){
+        return " ";
+    }
+}
